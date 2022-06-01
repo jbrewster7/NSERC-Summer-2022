a318708b4a66b45a8432268917a8d3346072e743
Josephine Brewster <josephinebrewster7@gmail.com>
2022-06-01 18:14:01 +0000

    updates

---
+++ Testing/dose_calculator_tests.py
@@ -0,0 +1,14 @@
+import unittest
+import siddon as sd
+
+class TestSiddonAlgorithm(unittest.TestCase):
+    
+    def setUp(self):
+        self.parameters = {}
+        self.parameters['num_planes'] =
+        self.parameters['voxel_lengths'] = 
+        self.parameters['beam_coor'] =
+        self.parameters['ini_planes'] =
+    
+    def test_distance_in_simple_cases(self):
+        self.assertEqual()
