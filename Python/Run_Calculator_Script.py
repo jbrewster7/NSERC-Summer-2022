383b9329c33abfc96bde8ba576b971bf53b103d9
Brewster, Josephine <josephinebrewster7@gmail.com>
2022-08-15 15:36:10 -0700

    u

--- Python/Run_Calculator_Script.py
+++ Python/Run_Calculator_Script.py
@@ -1,3 +1,43 @@
+import dose_calculator as dc
+import pickle
+import numpy as np
+
+def coorform(form,arr):
+    '''
+    Parameters:
+    ----------
+    form :: str
+      what form you want to have the return array in
+      (i.e. 'x', 'y', 'z' correspond to x first, ...)
+    
+    arr :: numpy array
+      array of topas voxels
+    
+    Returns:
+    -------
+    newarr :: numpy array
+    '''
+    newarr = []
+    if form == 'x':
+        newarr = arr
+    elif form == 'y':
+        for j in range(len(arr[0])):
+            plt_y = []
+            for n in range(len(arr)):
+                plt_y.append(arr[n][j])
+            newarr.append(plt_y)
+    elif form == 'z':
+        for k in range(len(arr[0][0])):
+            plt_z = []
+            for j in range(len(arr[0])):
+                plt_z.append([])
+                for i in range(len(arr)):
+                    plt_z[j].append(arr[i][j][k])
+            newarr.append(plt_z)
+    else:
+        raise ValueError('Invalid value for form. Only allowed values are \'x\', \'y\', \'z\'')
+    return np.array(newarr)
+
 # for a CT array of (Nx-1,Ny-1,Nz-1) voxels
 Nx = 229
 Ny = 131
@@ -22,8 +62,8 @@ yplane1 = -(Ny-1)*dy/2 - 7
 zplane1 = -(Nz-1)*dz/2 + 1.75
 
 # beam info and filename
-# beam_energy = [0.120] # in MeV
-# fluence_0 = [3.183098862 * 10**8] # photon/cm^2
+beam_energy = [0.120] # in MeV
+fluence_0 = [3.183098862 * 10**8] # photon/cm^2
 # fluence_0 = 2.53 * 10**8 # photon/cm^2
 # fluence_0 = 9.93 * 10**8 # photon/cm^2
 # filename = 'energy_absorption_coeff.txt'
@@ -49,7 +89,7 @@ for line in fo:
 densities = np.reshape(myArray2,[228,130,115],order='f').T
 densities = coorform('z',densities)
 
-print(shape(densities))
+print(np.shape(densities))
 
 # file names for energy absorption coefficients
 filenames = ['energy_absorption_coeffs_air.txt','energy_absorption_coeffs.txt','energy_absorption_coeffs_lung.txt','rib_bone_coeffs.txt']
@@ -75,10 +115,10 @@ for line in fo:
             myArray.append(int(n))
             
 materials = np.reshape(myArray[:],[228,130,115],order='f').T
-materials = array(materials,dtype=int)-1
+materials = np.array(materials,dtype=int)-1
 materials = coorform('z',materials)
 
-print(shape(materials))
+print(np.shape(materials))
 
 # number of cores to use
 num_cores = 16
@@ -86,5 +126,7 @@ num_cores = 16
 # Siddon((Nx,Ny,Nz),(dx,dy,dz),((x1,x2),(y1,y2),(z1,z2)),(xplane1,yplane1,zplane1),plot=True)
 # terma = sd.TERMA((Nx,Ny,Nz),(dx,dy,dz),((x1,x2),(y1,y2),(z1,z2)),(xplane1,yplane1,zplane1),[beam_energy],[fluence_0],mu)
 
-# dose = dc.Dose_Calculator((Nx,Ny,Nz),(dx,dy,dz),[((x1+adjust,x2+adjust),(y1+adjust,y2+adjust),(z1,z2)),((x1-adjust,x2-adjust),(y1+adjust,y2+adjust),(z1,z2)),((x1+adjust,x2+adjust),(y1-adjust,y2-adjust),(z1,z2)),((x1-adjust,x2-adjust),(y1-adjust,y2-adjust),(z1,z2))],(xplane1,yplane1,zplane1),beam_energy,fluence_0,angle_spread,position_spread,densities,filenames,kernelnames,kernel_size,eff_dist,materials,num_cores)
-dose = dc.Dose_Calculator((Nx,Ny,Nz),(dx,dy,dz),[((x1+adjust,x2+adjust),(y1+adjust,y2+adjust),(z1,z2))],(xplane1,yplane1,zplane1),beam_energy,fluence_0,angle_spread,position_spread,densities,filenames,kernelnames,kernel_size,eff_dist,materials,num_cores)
+dose = dc.Dose_Calculator((Nx,Ny,Nz),(dx,dy,dz),[((x1+adjust,x2+adjust),(y1+adjust,y2+adjust),(z1,z2)),((x1-adjust,x2-adjust),(y1+adjust,y2+adjust),(z1,z2)),((x1+adjust,x2+adjust),(y1-adjust,y2-adjust),(z1,z2)),((x1-adjust,x2-adjust),(y1-adjust,y2-adjust),(z1,z2))],(xplane1,yplane1,zplane1),beam_energy,fluence_0,angle_spread,position_spread,densities,filenames,kernelnames,kernel_size,eff_dist,materials,num_cores)
+#dose = dc.Dose_Calculator((Nx,Ny,Nz),(dx,dy,dz),[((x1+adjust,x2+adjust),(y1+adjust,y2+adjust),(z1,z2))],(xplane1,yplane1,zplane1),beam_energy,fluence_0,angle_spread,position_spread,densities,filenames,kernelnames,kernel_size,eff_dist,materials,num_cores)
+
+pickle.dump(dose,open('lungpatient_2.pickle','wb'))
